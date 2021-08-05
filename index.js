1a08149a1eb208aabbca9a028a1220d2e2885149
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-08-05 11:36:49 +0200

    standard --fix

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const yargs = require('yargs/yargs');
-const { logger, logConfig } = require('@vtfk/logger');
+const yargs = require('yargs/yargs')
+const { logger, logConfig } = require('@vtfk/logger')
 
 const getArgs = require('./lib/get-args')
-const moveFiles = require('./lib/move-files');
+const moveFiles = require('./lib/move-files')
 
 const args = getArgs(yargs(process.argv.slice(2)).argv)
 
 // check for required arguments
 if (!args.errorPath || !args.queuePath || !args.service) {
-    logger('error', [args.service, 'Required arguments are missing. Check out the readme!']);
-    return;
+  logger('error', [args.service, 'Required arguments are missing. Check out the readme!'])
+  process.exit(1)
 }
 
 logConfig({
-    prefix: args.service
+  prefix: args.service
 })
 
-logger('info', ['index', 'start']);
+logger('info', ['index', 'start'])
 
 // move files from errorPath to queuePath taking retryCount into account
 moveFiles(args)
-    .then(() => logger('info', ['index', 'finished']))
-    .catch(err => logger('err', ['index', 'finished', err]));
+  .then(() => logger('info', ['index', 'finished']))
+  .catch(err => logger('err', ['index', 'finished', err]))
