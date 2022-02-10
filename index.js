6275aa2c002441685d91981a0eacfec091e15da5
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-02-10 12:56:50 +0100

    Updated logger to v4.1.2

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 (async () => {
   const yargs = require('yargs/yargs')
-  const { logger, logConfig } = require('@vtfk/logger')
 
   const getArgs = require('./lib/get-args')
   const moveFiles = require('./lib/move-files')
+
+  const { logger, logConfig } = require('@vtfk/logger')
 
   const args = getArgs(yargs(process.argv.slice(2)).argv)
 
   // check for required arguments
   if (!args.errorPath || !args.queuePath || !args.service) {
-    logger('error', [args.service, 'Required arguments are missing. Check out the readme!'])
+    await logger('error', [args.service, 'Required arguments are missing. Check out the readme!'])
     process.exit(1)
   }
 
@@ -19,12 +20,12 @@
 
   // don't start if retryCount is less than, equal to 0 or undefined
   if (args.retryCount === undefined || args.retryCount <= 0) {
-    logger('warn', ['index', 'retry not enabled for this service', 'exiting...'])
+    await logger('warn', ['index', 'retry not enabled for this service', 'exiting...'])
     process.exit(0)
   }
 
-  logger('info', ['index', 'start'])
+  await logger('info', ['index', 'start'])
   await moveFiles(args)
-  logger('info', ['index', 'finished'])
+  await logger('info', ['index', 'finished'])
   process.exit(0)
 })()
